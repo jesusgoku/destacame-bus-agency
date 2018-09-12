bc8bab931506512594b46cf3ab2658cc1ce6d952
Jesús Urrutia <jesus.urrutia@gmail.com>
2018-09-12 02:40:55 -0300

    Extend from ModelSerializer

--- apps/control/serializers.py
+++ apps/control/serializers.py
@@ -3,30 +3,30 @@ from rest_framework import serializers
 from . import models
 
 
-class BusSerializer(serializers.HyperlinkedModelSerializer):
+class BusSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = models.Bus
         fields = ('id', 'capacity', 'identifier',)
 
 
-class DriverSerializer(serializers.HyperlinkedModelSerializer):
+class DriverSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = models.Driver
         fields = ('id', 'name', 'phone', 'email',)
 
-class RouteSerializer(serializers.HyperlinkedModelSerializer):
+class RouteSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = models.Route
         fields = ('id', 'name', 'duration',)
 
 
-class ItinerarySerializer(serializers.HyperlinkedModelSerializer):
+class ItinerarySerializer(serializers.ModelSerializer):
     class Meta(object):
         model = models.Itinerary
         fields = ('id', 'route', 'bus', 'driver', 'start_time', 'end_time', 'capacity', 'capacity_sold', 'duration')
 
 
-class PassengerSerializer(serializers.HyperlinkedModelSerializer):
+class PassengerSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = models.Passanger
         fields = ('id', 'itinerary', 'position', 'name', 'phone', 'emergency_name', 'emergency_phone',)
