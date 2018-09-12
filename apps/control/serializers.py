86577b4c92880c1010314b22b1d6de425b76779c
Jesús Urrutia <jesus.urrutia@gmail.com>
2018-09-12 03:22:25 -0300

    Create custom serializer support run model clean validations

--- apps/control/serializers.py
+++ apps/control/serializers.py
@@ -1,32 +1,45 @@
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
 
 from . import models
 
 
-class BusSerializer(serializers.ModelSerializer):
+class ModelCleanValidateSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        instance = self.Meta.model(**data)
+
+        try:
+            instance.clean()
+        except ValidationError as e:
+            raise serializers.ValidationError(e.args[0])
+
+        return data
+
+
+class BusSerializer(ModelCleanValidateSerializer):
     class Meta(object):
         model = models.Bus
         fields = ('id', 'capacity', 'identifier',)
 
 
-class DriverSerializer(serializers.ModelSerializer):
+class DriverSerializer(ModelCleanValidateSerializer):
     class Meta(object):
         model = models.Driver
         fields = ('id', 'name', 'phone', 'email',)
 
-class RouteSerializer(serializers.ModelSerializer):
+class RouteSerializer(ModelCleanValidateSerializer):
     class Meta(object):
         model = models.Route
         fields = ('id', 'name', 'duration',)
 
 
-class ItinerarySerializer(serializers.ModelSerializer):
+class ItinerarySerializer(ModelCleanValidateSerializer):
     class Meta(object):
         model = models.Itinerary
         fields = ('id', 'route', 'bus', 'driver', 'start_time', 'end_time', 'capacity', 'capacity_sold', 'duration')
 
 
-class PassengerSerializer(serializers.ModelSerializer):
+class PassengerSerializer(ModelCleanValidateSerializer):
     class Meta(object):
         model = models.Passanger
         fields = ('id', 'itinerary', 'position', 'name', 'phone', 'emergency_name', 'emergency_phone',)
